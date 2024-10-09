a0bb699d7b7f735a11dd9eda14887f8b7bebb35a
chloe-ui218 <chloerhinan10@gmail.com>
2024-10-08 14:55:36 -1200

    feat: added local storage for data handling

--- To-Do/script.js
+++ To-Do/script.js
@@ -1,9 +1,10 @@
 let tasks = ["Buy milk", "Clean the room", "Go to the gym"];
 
 
+
 const displayTasks = () => {
-    let taskDisplay = document.querySelector("#taskDisplay");
-    taskDisplay.innerHTML = "";
+    let taskDisplay = document.querySelector('#taskDisplay');
+    taskDisplay.innerHTML = '';
     tasks.forEach((task, index) => {
         const taskItem = document.createElement('li');
         taskItem.classList.add('bg-blue-200', 'p-2', 'm-2', 'rounded-lg', 'decoration-none', 'list-none');
@@ -11,13 +12,18 @@ const displayTasks = () => {
         taskDisplay.appendChild(taskItem);
     })
 }
+
+const saveTaskToLocalStorage = () => {
+    localStorage.setItem('tasks', JSON.stringify(tasks)); 
+}
 const addTask = () => {
     const newTaskInput = document.querySelector('#newTask');
     const newTask = newTaskInput.value;
 
-    if(newTask.trim !== ""){
+    if(newTask.trim() !== ""){
         tasks.push(newTask);
         newTaskInput.value = '';
+        saveTaskToLocalStorage();
         displayTasks();
     } else {
         alert('Please enter a task');
@@ -26,5 +32,16 @@ const addTask = () => {
 
 const addTaskButton = document.querySelector('#addTaskButton');
 addTaskButton.addEventListener('click', addTask);
+
+const loadTasksFromLocalStorage = () => {
+    const storedTasks = localStorage.getItem('tasks');
+
+    if(storedTasks){
+        tasks = JSON.parse(storedTasks);
+        displayTasks();
+    }
+}
+
+loadTasksFromLocalStorage();
 
 displayTasks();
