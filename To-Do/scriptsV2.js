ed541cc58862bbe481fa5d71b1a1c0c51e2b5eb4
chloe-ui218 <chloerhinan10@gmail.com>
2024-10-10 06:03:50 -1200

    feat: functioning update button

--- To-Do/scriptsV2.js
+++ To-Do/scriptsV2.js
@@ -4,23 +4,33 @@ const displayTasks = () => {
     let taskDisplay = document.querySelector('#taskDisplay');
     taskDisplay.innerHTML = '';
     tasks.forEach((task, index) => {
+
+        // create the task container
         const taskItem = document.createElement('li');
         taskItem.classList.add('bg-blue-200', 'p-2', 'm-2', 'rounded-lg', 'flex', 'justify-between', 'list-none');
 
+
+        // create the tasks title container
         const taskText = document.createElement('span');
         taskText.textContent = task;
         taskItem.appendChild(taskText);
-        // taskItem.textContent = task;
 
+
+        // create the tasks links container
         const taskLinks = document.createElement('div');
         taskLinks.classList.add('task-links');
 
+
+        // create the update and delete buttons
         const updateButton = document.createElement('a');
         updateButton.href = '#';
         updateButton.textContent = 'Update';
         updateButton.classList.add('text-blue-500' , 'mr-4');
+        updateButton.addEventListener('click', () => editTask(index));
         taskLinks.appendChild(updateButton);
         
+
+
 
         const deleteButton = document.createElement('a');
         deleteButton.href = '#';
@@ -53,6 +63,18 @@ const addTask = () => {
     }
 }
 
+const editTask = (index) => {
+    const updatedTask = prompt('Enter the updated task', tasks[index]);
+    if (upddatedTask && updatedTask.trim() !== '') {
+        tasks[index] = updatedTask.trim();
+        saveTaskToLocalStorage();
+       displayTasks();
+    } else{
+        alert('Please enter a valid task');
+    }
+
+}
+
 const deleteTask = (index) => {
     if (confirm('Are you sure you want to delete this task?')){
         tasks.splice(index, 1);
@@ -75,4 +97,3 @@ const addTaskButton = document.querySelector('#addTaskButton');
 addTaskButton.addEventListener('click', addTask);
 
 loadTaskFromLocalStorage();
-displayTasks();
