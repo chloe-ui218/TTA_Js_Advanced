6b9a559b499e0fd9aa9a25ea7a9cd14459651484
chloe-ui218 <chloerhinan10@gmail.com>
2024-12-10 17:18:14 -1200

    feat: enhanced the UI

--- data_storage/JSON/API/async_await/scripts.js
+++ data_storage/JSON/API/async_await/scripts.js
@@ -1,22 +1,24 @@
-async function fetchPsts() {
+async function fetchPosts() {
   try {
-    const response = await fetch ('https://jsonplaceholder.typicode.com/posts');
-    postData = await response.json();
-    console.log(data);
-   const postDiv = document.getElementById('posts');
-   data.forEach(post => {
-       const postElement = document.createElement('div');
-       postElement.classList.add('post');
-       postElement.innerHTML = `
-       <h2>${post.title}</h2>
-       <p>${post.body}</p>
-       `;
-       postDiv.appendchild(postElement);
-   });
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const postData = await response.json(); 
+    console.log(postData); 
+    
+    const postDiv = document.getElementById('posts');
+    postData.forEach(post => {
+      const postElement = document.createElement('div');
+      postElement.classList.add('post');
+      postElement.innerHTML = `
+        <h2>${post.title}</h2>
+        <p>${post.body}</p>
+      `;
+      postDiv.appendChild(postElement); 
+    });
   } catch (error) {
     console.log('Error:', error);
     document.getElementById('posts').innerHTML = '<h3 style="color: red;">Sorry, something went wrong!</h3>';
   }
-
 }
-fetchPsts;
+
+
+fetchPosts();
